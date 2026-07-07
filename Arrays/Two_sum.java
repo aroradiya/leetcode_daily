02c951986a565ede38b8a9d89f02aacb985b106a
aroradiya <diya2721.beai24@chitkara.edu.in>
2026-07-08 01:53:11 +0530

    add binary search

---
+++ Arrays/Two_sum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int result[]=new int[2];
+
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                result[0]=i;
+                result[1]=map.get(target-nums[i]);
+                return result;
+            }
+            else{
+                map.put(nums[i],i);    //key value -> element and its indice
+            }
+
+        }
+        return result;
+    }
+}

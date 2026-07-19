203384e41256fbf27fea37159d60b45bd184d7bd
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-07-19 14:16:28 +0530

    Time: 376 ms (99.06%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
-select (author_id) as id
+-- select (author_id) as id
+-- from views
+-- group by author_id,viewer_id 
+-- having author_id=viewer_id 
+-- order by author_id asc;
+
+select distinct(author_id) as id
 from views
-group by author_id,viewer_id 
-having author_id=viewer_id 
+where author_id=viewer_id
 order by author_id asc;

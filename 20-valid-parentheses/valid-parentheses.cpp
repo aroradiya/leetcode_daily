2290eddbb3d7adf54a67d9bc5948796b7fbb9c3c
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-07-20 23:51:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.22%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{'|| s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                char ch=st.top(); 
+               
+                if((s[i]==')'&& ch!='(')|| (s[i]=='}'&& ch!='{')||(s[i]==']'&& ch!='[')){
+                    return false;
+                }
+                st.pop();
+               
+            }
+        }
+        return st.empty(); //equivalent to agr empty->false else true
+    }
+};

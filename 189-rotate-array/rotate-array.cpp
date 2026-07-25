4fbaeddce64b4768376ee1337162d2049bee6c78
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-07-25 13:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (70.31%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+
+        int n=nums.size();
+        k=k%n;
+
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     int size=nums.size();
+    //     k%=size;
+    
+       
+    //     for(int i=0;i<k;i++){
+    //         int last=nums[size-1];
+            
+    //         for(int j=size-1;j>0;j--){
+    //             nums[j]=nums[j-1];
+    //         }
+        
+    //     nums[0]=last;
+    //     }
+        
+    // }
+    
+    //};

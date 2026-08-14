93415481176c7cc0f850c5e8ea895728f987d14a
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-08-14 21:19:43 +0530

    Time: 2 ms (98.36%) | Memory: 52.5 MB (66.71%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        int max=0;
+        for(int i=0;i<n;i++){
+          if(nums[i]==0){
+            count=0;
+          }
+          else{
+            count++;
+            if(count>max){
+                max=count;
+            }
+          }
+          
+        }
+        return max;
+    }
+}

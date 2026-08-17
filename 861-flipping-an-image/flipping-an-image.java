1802385adc155d8e1e0614b0e4af8b87e65ed45f
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-08-17 21:54:13 +0530

    Time: 1 ms (47.67%) | Memory: 45.6 MB (73.72%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int m=image.length;
+        int n=image[0].length;
+        for(int i=0;i<m;i++){
+            int left=0;
+            int right=n-1;
+            while(left<right){
+                int temp=image[i][left];
+                image[i][left]=image[i][right];
+                image[i][right]=temp;
+
+                left++;
+                right--;
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+              image[i][j]=1-image[i][j];
+            }
+        }
+        return image;
+      
+     
+    }
+}

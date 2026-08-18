b2495ade51347e8423083e467b445e9272b3f312
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-08-18 12:45:50 +0530

    Time: 2 ms (60.87%) | Memory: 42.8 MB (96.30%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,15 +1,26 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()){
-            return false;
-        }
-        String doubles=s+s;
-        if(doubles.contains(goal)){
-            return true;
-        }
-        else{
-            return false;
-        }
+        // if(s.length()!=goal.length()){
+        //     return false;
+        // }
+        // String doubles=s+s;
+        // if(doubles.contains(goal)){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+
+        return s.length()==goal.length() &&(s+s).contains(goal);
+
+
+
+
+
+
+
+
+
         // int last=s[len-1];
         // for(int i=1;i<len;i++){
         //     s[i-1]=s[i];

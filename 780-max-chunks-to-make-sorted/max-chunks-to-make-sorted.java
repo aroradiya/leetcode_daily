f225e3a899e082d4ce58980b6497c813bcf6c864
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-08-21 11:56:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (84.62%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,14 +1,30 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
+        // int n=arr.length;
+        // int running_max=0;
+        // int chunks=0;
+        // for(int i=0;i<n;i++){
+        //     running_max=Math.max(running_max,arr[i]);
+        //     if(running_max==i){
+        //         chunks++;
+        //     }
+        // }
+        // return chunks;
         int n=arr.length;
-        int running_max=0;
-        int chunks=0;
+        Deque<Integer> st=new ArrayDeque<>();
         for(int i=0;i<n;i++){
-            running_max=Math.max(running_max,arr[i]);
-            if(running_max==i){
-                chunks++;
+            int curr=arr[i];
+            if(st.isEmpty()|| curr>=st.peek()){
+                st.push(curr);
+            }
+            else{
+                int max=st.pop();
+                while(!st.isEmpty() && curr<st.peek()){
+                    st.pop();
+                }
+                st.push(max);
             }
         }
-        return chunks;
+        return st.size();
     }
 }

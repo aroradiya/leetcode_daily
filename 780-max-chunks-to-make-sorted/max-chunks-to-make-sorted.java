2b5c346fc9eaf5ef526dd5d2a7140bc210b1423f
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-08-21 11:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (84.62%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,30 +1,37 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        // int n=arr.length;
-        // int running_max=0;
-        // int chunks=0;
-        // for(int i=0;i<n;i++){
-        //     running_max=Math.max(running_max,arr[i]);
-        //     if(running_max==i){
-        //         chunks++;
-        //     }
-        // }
-        // return chunks;
+        //running max approach
         int n=arr.length;
-        Deque<Integer> st=new ArrayDeque<>();
+        int running_max=0;
+        int chunks=0;
         for(int i=0;i<n;i++){
-            int curr=arr[i];
-            if(st.isEmpty()|| curr>=st.peek()){
-                st.push(curr);
-            }
-            else{
-                int max=st.pop();
-                while(!st.isEmpty() && curr<st.peek()){
-                    st.pop();
-                }
-                st.push(max);
+            running_max=Math.max(running_max,arr[i]);
+            if(running_max==i){
+                chunks++;
             }
         }
-        return st.size();
+        return chunks;
+
+        //monotonic stack
+
+
+
+
+        // int n=arr.length;
+        // Deque<Integer> st=new ArrayDeque<>();
+        // for(int i=0;i<n;i++){
+        //     int curr=arr[i];
+        //     if(st.isEmpty()|| curr>=st.peek()){
+        //         st.push(curr);
+        //     }
+        //     else{
+        //         int max=st.pop();
+        //         while(!st.isEmpty() && curr<st.peek()){
+        //             st.pop();
+        //         }
+        //         st.push(max);
+        //     }
+        // }
+        // return st.size();
     }
 }

d05c75a5d50fd5918894ff047feafe839a17088b
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-09-01 23:23:50 +0530

    Time: 7 ms (22.13%) | Memory: 45.3 MB (38.09%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -1,22 +1,32 @@
 class Solution {
     public int findLucky(int[] arr) {
         int n=arr.length;
-        ArrayList<Integer> list=new ArrayList<>();
+        //ArrayList<Integer> list=new ArrayList<>();
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<n;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        for(int i=0;i<n;i++){
-            if(arr[i]==map.get(arr[i])){
-                list.add(arr[i]);
-            }
-        }
-        int max=-1;
-        for(int i=0;i<list.size();i++){
-            if(list.get(i)>max){
-                max=list.get(i);
+        int max = -1;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == map.get(arr[i])){
+            max = Math.max(max, arr[i]);
             }
         }
         return max;
+
+
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]==map.get(arr[i])){
+        //         list.add(arr[i]);
+        //     }
+        // }
+        // int max=-1;
+        // for(int i=0;i<list.size();i++){
+        //     if(list.get(i)>max){
+        //         max=list.get(i);
+        //     }
+        // }
+        // return max;
     }
 }

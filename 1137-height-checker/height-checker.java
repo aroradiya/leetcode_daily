1a445397f980037b9b507e2a2048972af740000a
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-09-11 23:34:12 +0530

    Time: 4 ms (77.88%) | Memory: 43.5 MB (27.27%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -5,16 +5,17 @@ class Solution {
         for(int i=0;i<n;i++){
             expected[i]=heights[i];
         }
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-1-i;j++){
-                if(expected[j]>expected[j+1]){
-                    int temp=expected[j];
-                    expected[j]=expected[j+1];
-                    expected[j+1]=temp;
-                }
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=0;j<n-1-i;j++){
+        //         if(expected[j]>expected[j+1]){
+        //             int temp=expected[j];
+        //             expected[j]=expected[j+1];
+        //             expected[j+1]=temp;
+        //         }
 
-            }
-        }
+        //     }
+        // }
+        Arrays.sort(expected);
         int count=0;
         for(int i=0;i<n;i++){
             if(heights[i]!=expected[i]){

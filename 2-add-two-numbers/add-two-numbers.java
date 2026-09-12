7b6d94f9e0cb6526314ca6b1e02f7a4596db2d79
Diya Arora <diya2721.beai24@chitkara.edu.in>
2026-09-12 23:13:36 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (77.02%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -13,7 +13,7 @@ class Solution {
         int carry=0;
         ListNode dummy=new ListNode(-1);
         ListNode curr=dummy;
-        while(l1!=null|| l2!=null||carry!=0){
+        while(l1!=null || l2!=null||carry!=0){
             int sum=carry;
             if(l1!=null){
                 sum+=l1.val;
@@ -27,8 +27,7 @@ class Solution {
             carry=sum/10;
             curr.next=newNode;
             curr=curr.next;
-        }
+        } 
         return dummy.next;
-        
     }
 }
